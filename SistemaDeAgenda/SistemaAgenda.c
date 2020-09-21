c1541d6e69a43cb5c8ea5c7e382d2b2abad06e83
Eduardo Catunda <EduCatunda@users.noreply.github.com>
2020-09-21 19:22:51 -0300

    Atualização do sistema de agenda

--- SistemaDeAgenda/SistemaAgenda.c
+++ SistemaDeAgenda/SistemaAgenda.c
@@ -9,22 +9,29 @@
 #define COL 51
 #define MAX 50
 
+void alterar(int numCadastro);
 int existeArquivo(char *cpfileName);
 void inserir();
 void listar();
 void menu();
+int obterNumCadastro();
 /**
 - o sistema deverá ter um menu de inserção, alteração e deleção de cadastros;
 - deverá permitir também consulta aos dados por nome, cidade ou estado;
-- os dados ficarão gravados em arquivo Texto;
-- os cadastros deverão conter: nome, endereço, bairro, cidade, estado, cep, telefone e celular.
+- os dados ficarão gravados em arquivo Texto; - CONFERE
+- os cadastros deverão conter: nome, endereço, bairro, cidade, estado, cep, telefone e celular. - CONFERE
 */
 
 FILE *fp;
 char agendaTxt[] = "Agenda.txt";
 char agenda[ROW][COL];
 char dados[ROW][COL] = {"Nome", "Endereco", "Bairro", "Cidade", "Estado", "CEP", "Telefone", "Celular"};
-int numAgenda = 0;
+int numCadastro = 0;
+
+void alterar(int numCadastro)
+{
+
+}
 
 int existeArquivo(char *arquivo)
 {
@@ -40,11 +47,14 @@ void inserir()
     {
         fp = fopen(agendaTxt, "w");
         fprintf(fp, "Cadastro 1:\n");
+        fclose(fp);
     }
     else
     {
-        fp = fopen(agendaTxt, "w");
-        fprintf(fp, "Cadastro %d:\n", numAgenda);
+        ++numCadastro;
+        fp = fopen(agendaTxt, "a");
+        fprintf(fp, "\nCadastro %d:\n", numCadastro);
+        fclose(fp);
     }
     printf("Cadastre os seguintes dados:\n");
     char palavra[50];
@@ -59,12 +69,11 @@ void inserir()
     for (int j = 0; j < ROW; ++j)
     {
         if (j != ROW - 1)
-            fprintf(fp, "%s ", agenda[j]);     //Cadastro 1:
+            fprintf(fp, "%s\n", agenda[j]);     //Cadastro 1:
         else                                    //[Nome, Endereco, Bairro, Cidade, Estado, CEP, Telefone, Celular]
-            fprintf(fp, "%s]", agenda[j]);
+            fprintf(fp, "%s", agenda[j]);
 
     }
-    ++numAgenda;
     printf("\n\n");
     fclose(fp);
 }
@@ -77,17 +86,26 @@ void listar()
     if (!existeArquivo(agendaTxt))
         printf("Nao ha nenhum dado cadastrado!\n");
     else
-    {
-        for (int i = 0; i < COL; ++i) //(fgets(buffer, COL, fp)!= NULL)
-        {
-            if (i)
-                printf("%s: %s\n", dados[i], buffer);
-            else
-                fgets(buffer, COL, fp
-            printf("%s\n", buffer);
+        for (int i = 0; i < numCadastro; ++i){
+            for (int j = 0; j <= ROW; ++j){
+                fgets(buffer, COL, fp);
+                if (j)
+                    printf("%s: %s", dados[j-1], buffer);
+                else
+                    printf("%s", buffer);
+            }
+            printf("\n\n");
         }
-    }
-    printf("\n\n");
+    fclose(fp);
+}
+
+int main ()
+{
+    numCadastro = obterNumCadastro();
+    printf("Num Cadastro: %d\n", numCadastro);
+    menu();
+
+    return 0;
 }
 
 void menu ()
@@ -137,10 +155,36 @@ void menu ()
     }
 }
 
-int main ()
+///Obtem o último número de cadastro.
+int obterNumCadastro()
 {
-    menu();
+    if (existeArquivo(agendaTxt)){ //Verifica se existe o arquivo
+        fp = fopen(agendaTxt, "r"); // Abrindo o arquivo para leitura
+        char substring[COL] = "Cadastro", buff[255], modelCad[20]; //Cadastro []:
+        while (fgets(buff, 255, (FILE*)fp) != NULL) // Lendo todo o arquivo (até chegar à última linha)
+            if((strstr(buff, substring)) != NULL) // Verifica se a linha atual possui o mesmo conteúdo da substring
+                strcpy(modelCad, buff); // Guarda a linha que possui a última aparência da substring
 
-    return 0;
+        int tamanho = strlen(modelCad), posDoisPontos;
+        char doisPontos = ':';
+        for (int i = 0; i < tamanho; ++i){ // Encontra a posição dos dois pontos (:) na linha
+            if (modelCad[i] == doisPontos)
+                posDoisPontos = i;
+        }
+
+        int posFimCad = 9, tamNumero = posDoisPontos - posFimCad; // Com a posição dos dois pontos encontrada,
+        char numCadChar[tamNumero];                               // é possível identificar a posição do número do cadastro
+        for (int i = posFimCad, j = 0; i < posDoisPontos; ++i, ++j){
+            numCadChar[j] = modelCad[i];
+        }
+        fclose(fp); // Foi encontrado o array do número do último cadastro
+
+        int numCadInt[tamNumero], numCad;                            // Transforma cada caractere do array em inteiro
+        for (int i = tamNumero - 1, j = 1; i >= 0; --i, j = j * 10){ // por meio do método char c = '1'; int a = c - '0';
+            numCadInt[i] = (numCadChar[i] - '0')*j; // Multiplicará pela base em que se encontra. 13 = 3 * 10^0 + 1 *10^1
+            numCad += numCadInt[i];
+        }
+        return numCad; // Retorna o inteiro
+    }
+    return 1;
 }
-,
