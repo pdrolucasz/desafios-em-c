3ae1d86d345617d2b3dcec6bb58fa0b059fbd02a
Eduardo Catunda <EduCatunda@users.noreply.github.com>
2020-09-28 01:02:34 -0300

    Incrementa o método de alterar dados/Melhoramento dos métodos de busca.

--- SistemaDeAgenda/SistemaAgenda.c
+++ SistemaDeAgenda/SistemaAgenda.c
@@ -9,11 +9,12 @@
 #define COL 51
 #define MAX 50
 
-void alterar(int numCadastro);
+int altera(char dado[COL], int corrente, int ehUltimo);
+int alterarCadastro(char numCad[4]);
 void buscarCidade(char dado[COL]);
 void buscarEstado(char dado[COL]);
 void buscarNome(char dado[COL]);
-int existeArquivo(char *cpfileName);
+int existeArquivo(char *nomeArquivo);
 void inserir();
 void listarTudo();
 void listarPorCadastro(char cadastro[20]);
@@ -31,66 +32,92 @@ FILE *fp, *fs; // File principal e secundário
 char agendaTxt[] = "Agenda.txt";
 char agenda[ROW][COL];
 char dados[ROW][COL] = {"Nome", "Endereco", "Bairro", "Cidade", "Estado", "CEP", "Telefone", "Celular"};
-int numCadastro = 0;
+int ultimoCad = 0;
 
-/// Altera um dado cadstrado do arquivo de acordo com o número do cadastro
-void alterar(int numCadastro)
+/// Sobreescreve um dado de um arquivo que esteja presente no byte corrente
+int altera(char dado[COL], int corrente, int ehUltimo)
 {
-    if (!existeArquivo(agendaTxt))
-        printf("Nao ha nenhum dado cadastrado para alterar!\n");
-    else
-    {
-    int qualCad;
-    printf("Qual cadastro deseja alterar?"); scanf("%d", &qualCad);
-    while (qualCad < 1 || qualCad > 5){
-            printf("\nOpcao invalida! Escolha entre 1 e %d\n\n", numCadastro);
-            scanf("%d", &qualCad);
-    }
+    int tamDadoAnt, tamDado = strlen(dado);
+    char vazio[3] = " ", buff[COL];
 
-    }
+    fp = fopen(agendaTxt, "r");
+    fseek(fp, corrente, SEEK_SET); // Procurando pelo dado que deve ser alterado.
+    fgets(buff, COL, fp);
+    tamDadoAnt = strlen(buff);
+    if (tamDadoAnt > tamDado)
+        for (int i = 0; i < tamDadoAnt - tamDado; ++i)
+            strcat(dado, vazio); // Para alterar um dado, é necessário preencher todo o espaço que o antigo dado possui.
+    fclose(fp);
+
+    fp = fopen(agendaTxt, "r+b"); // Esse modo de abertura permite que o arquivo seja sobreescrito.
+    fseek(fp, corrente, SEEK_SET); // Pesquisa-se a linha que deve ser sobreescrita com um novo dado
+    if (!ehUltimo)
+        fprintf(fp, "%s\n", dado);
+    else
+        fprintf(fp, "%s", dado);
+    fclose(fp);
+    corrente += tamDadoAnt + 1;
+    return corrente;
 }
 
-void buscarCidade(char dado[COL])
+///Verifica se o número de cadastro passado existe e, caso verdadeiro, altera os dados dele.
+int alterarCadastro(char numCad[4])
+{
+    if (!existeArquivo(agendaTxt))
+        return 0;
+    char linhaCad[] = "Cadastro ", buff[COL];
+    int corrente = 0;
+    strcat(linhaCad, strcat(numCad, ":\n"));
+    while (fgets(buff, COL, fp) != NULL)
+    {
+        corrente += strlen(buff) + 1;
+        if (strcmp(buff, linhaCad) == 0){ // Encontra a linha do cadastro.
+            printf("Altere os seguintes dados:\n", buff);
+            char palavra[50];
+            for (int linha = 0; linha < 8; ++linha){ // Lê todas as 8 linhas seguintes (dados do cadastro)
+                printf("\n%s: ", dados[linha]); gets(palavra);
+                if (linha == 7) corrente = altera(palavra, corrente, 0);
+                else corrente = altera(palavra, corrente, 1);
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/// Busca pelo Dado passado pelo usuário de acordo com o inteiro linhaDado: 1 - Nome, 4 - Cidade, 5 - Estado
+void buscar(char dado[COL], int linhaDado)
 {
     if (existeArquivo(agendaTxt)){
         fp = fopen(agendaTxt, "r");
         char buff[COL], linhaCad[20];
-        int linha = 0, corrente = 0;
+        int linha = 0, corrente = 0, encontrou = 0, tamDado = strlen(dado) - 1;
         while (fgets(buff, COL, (FILE*)fp) != NULL)
         {
             corrente += strlen(buff) + 1;
             if(linha % 9 == 0) // É linha de cadastro "Cadastro []:".
                 strcpy(linhaCad, buff);
 
-            else if (linha >= 4 && (linha - 4) % 9 == 0) // É linha de Cidade.
-                if (strcmp(buff, dado) == 0){ // Verifica se a linha atual corresponde ao dado
+            else if (linha >= linhaDado && (linha - linhaDado) % 9 == 0) // É linha a qual se encontra o dado
+                if (strncmp(buff, dado, tamDado) == 0){ // Verifica se a linha atual corresponde ao dado
                     listarPorCadastro(linhaCad);
+                    ++encontrou;
                     fseek(fp, corrente, SEEK_CUR);
                 }
             ++linha;
         }
+        if (!encontrou)
+            printf("Nao foi encontrado nenhum cadastro com esse dado.\n\n");
         fclose(fp);
     }
     else
         printf("Nao ha nenhum dado cadastrado para ser listado!\n");
 }
 
-/// Busca pelo Estado passado pelo usuário
-void buscarEstado(char dado[COL])
-{
-
-}
-
-/// Busca pelo Nome passado pelo usuário
-void buscarNome(char dado[COL])
-{
-
-}
-
 /// Verifica se o arquivo existe
-int existeArquivo(char *arquivo)
+int existeArquivo(char *nomeArquivo)
 {
-    fp = fopen(arquivo, "r");
+    fp = fopen(nomeArquivo, "r");
     if (!fp)
         return 0; /* nao existe */
     return 1; /* existe */
@@ -107,9 +134,9 @@ void inserir()
     }
     else
     {
-        ++numCadastro;
+        ++ultimoCad;
         fp = fopen(agendaTxt, "a");
-        fprintf(fp, "\nCadastro %d:\n", numCadastro);
+        fprintf(fp, "\nCadastro %d:\n", ultimoCad);
         fclose(fp);
     }
     printf("Cadastre os seguintes dados:\n");
@@ -142,7 +169,7 @@ void listarTudo()
     if (!existeArquivo(agendaTxt))
         printf("Nao ha nenhum dado cadastrado para ser listado!\n");
     else
-        for (int i = 0; i < numCadastro; ++i){
+        for (int i = 0; i < ultimoCad; ++i){
             for (int j = 0; j <= ROW; ++j){
                 fgets(buffer, COL, fp);
                 if (j)
@@ -184,8 +211,7 @@ void listarPorCadastro(char cadastro[20])
 /// Main
 int main ()
 {
-    numCadastro = obterNumCadastro();
-    printf("Ultimo numero de cadastro: %d\n", numCadastro);
+    ultimoCad = obterNumCadastro();
     menu();
 
     return 0;
@@ -197,6 +223,7 @@ void menu ()
     while (1)
     {
         int escolha;
+        char qualCad[4];
         printf("---------------------------------------------------\nAgenda\n---------------------------------------------------\n");
         printf("Escolha uma opcao.\n\n1 - Inserir novos cadastros\n\n2 - Listar banco de dados\n\n3 - Alterar um cadastro\n\n4 - Deletar dados cadastrados\n\n5 - Sair\n\n");
         scanf("%d", &escolha);
@@ -208,14 +235,14 @@ void menu ()
         switch (escolha)
         {
         case 1:
-            printf("\nOpcao (1) escolhida...\n\n");
+            printf("\nOpcao (1) escolhida...\n");
             Sleep(1000);
             system("cls");
             inserir();
             break;
 
         case 2:
-            printf("\nOpcao (2) escolhida...\n\n");
+            printf("\nOpcao (2) escolhida...\n");
             Sleep(1000);
             system("cls");
             menu2();
@@ -225,6 +252,9 @@ void menu ()
             printf("\nOpcao (3) escolhida...\n\n");
             Sleep(1000);
             system("cls");
+            printf("Qual o numero de cadastro deseja alterar? "); gets(qualCad);
+            if(!alterarCadastro(qualCad))
+                printf("Esse numero de cadastro nao existe.\n");
             break;
 
         case 4:
@@ -245,7 +275,7 @@ void menu2()
 {
     int escolha;
     char dado[COL];
-    printf("Escolha uma opcao.\n\n1 - Listar todo banco de dados\n\n2 - Buscar por nome\n\n3 - Buscar por cidade\n\n4 - Buscar por estado\n\n5 - Sair\n\n");
+    printf("Escolha uma opcao.\n\n1 - Listar todo banco de dados\n\n2 - Buscar por nome\n\n3 - Buscar por cidade\n\n4 - Buscar por estado\n\n5 - Voltar\n\n");
     scanf("%d", &escolha);
     while (escolha < 1 || escolha > 5){
         printf("\nOpcao invalida! Escolha entre 1 e 5\n\n");
@@ -267,7 +297,8 @@ void menu2()
         system("cls");
         printf("Digite o nome que deve ser buscado: ");
         gets(dado);
-        buscarNome(dado);
+        printf("\n", strcat(dado, "\n"));
+        buscar(dado, 1);
         break;
 
     case 3:
@@ -277,7 +308,7 @@ void menu2()
         printf("Digite a cidade que deve ser buscada: ");
         gets(dado);
         printf("\n", strcat(dado, "\n"));
-        buscarCidade(dado);
+        buscar(dado, 4);
         break;
 
     case 4:
@@ -286,7 +317,8 @@ void menu2()
         system("cls");
         printf("Digite o estado que deve ser buscado: ");
         gets(dado);
-        buscarEstado(dado);
+        printf("\n", strcat(dado, "\n"));
+        buscar(dado, 5);
         break;
 
     case 5:
@@ -298,6 +330,7 @@ void menu2()
 }
 
 /// Obtem o último número de cadastro.
+//TO DO: começar a ler o arquivo de baixo pra cima, pelo método fseek, passando a constante reservada SEEK_END
 int obterNumCadastro()
 {
     if (existeArquivo(agendaTxt)){
@@ -311,7 +344,7 @@ int obterNumCadastro()
             ++linha;
         }
         fclose(fp);
-        int tamCad = strlen(linhaCad), posDoisPontos = tamCad - 2;
+        int tamCad = strlen(linhaCad), posDoisPontos = tamCad - 2; // No fim de cada linha, há o caractere de endline (\n) e fim de string (\0).
         int posFimCad = 9, tamNumero = posDoisPontos - posFimCad, numCadInt[tamNumero]; // Com a posição dos dois pontos encontrada,
         char numCadChar[tamNumero];                                                     // é possível identificar o número do cadastro.
         strncpy(numCadChar, linhaCad + 9, tamNumero);
